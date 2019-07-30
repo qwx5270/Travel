84974e33f6dc3c732f99be87cdc6dfb696778a71
qwx <edward5270@163.com>
2019-07-30 13:33:28 +0800

    add city router

--- travel-project/src/pages/home/components/Header.vue
+++ travel-project/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe615;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe615;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -28,7 +30,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -51,6 +53,7 @@ export default {
       width: 1.25rem
       float: rigth
       text-align: center
+      color: #fff
       .arrow-icon
         font-size: .25rem
         margin-left: .06rem
