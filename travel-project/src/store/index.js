5ce48027bd58ba351152bdcbc676e373b94e960c
qwx <edward5270@163.com>
2019-08-01 13:55:48 +0800

    finished pages

--- travel-project/src/store/index.js
+++ travel-project/src/store/index.js
@@ -6,10 +6,5 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
-  mutations,
-  getters: {
-    doubleCity (city) {
-      return state.city + ' ' + state.city
-    }
-  }
+  mutations
 })
